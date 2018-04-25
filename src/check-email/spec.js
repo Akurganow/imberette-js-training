fe6204da70d5b82f02de60f8c8f68a5eb2693814
Alexander Kurganow <me@akurganow.ru>
2018-04-25 19:46:23 +0300

    feat(trainings): add query-to-object

--- src/check-email/spec.js
+++ src/check-email/spec.js
@@ -1,28 +1,28 @@
-import exp from './index'
+import check from './index'
 
 describe('Check email', () => {
   test('@email.com', () => {
-    expect(exp('@email.com')).toBe(false)
+    expect(check('@email.com')).toBe(false)
   })
 
   test('some@email', () => {
-    expect(exp('some@email')).toBe(false)
+    expect(check('some@email')).toBe(false)
   })
 
   test('some@.com', () => {
-    expect(exp('some@.com')).toBe(false)
+    expect(check('some@.com')).toBe(false)
   })
 
   test('some@email.com', () => {
-    expect(exp('some@email.com')).toBe(true)
+    expect(check('some@email.com')).toBe(true)
   })
 
   test('some+1@email.com', () => {
-    expect(exp('some+1@email.com')).toBe(true)
+    expect(check('some+1@email.com')).toBe(true)
   })
 
   test('so.me@email.com', () => {
-    expect(exp('so.me@email.com')).toBe(true)
+    expect(check('so.me@email.com')).toBe(true)
   })
 
 })
