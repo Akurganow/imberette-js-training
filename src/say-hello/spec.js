d64ab27b9de1cbc7ad35e8e632346d81d7e48748
Alexander Kurganow <me@akurganow.ru>
2018-05-01 21:36:56 +0300

    feat(trainings): add say-hello

---
+++ src/say-hello/spec.js
@@ -0,0 +1,23 @@
+import sayHello from './index'
+
+describe('Say hello', () => {
+  test('fullname', () => {
+    expect(sayHello({ name: 'Alex', lastname: 'Kurganov' })).toBe('Hello! Alex Kurganov')
+  })
+
+  test('only name', () => {
+    expect(sayHello({ name: 'Alex' })).toBe('Hello! Alex')
+  })
+
+  test('only lasname', () => {
+    expect(sayHello({ lastname: 'Kurganov' })).toBe('Hello! Kurganov')
+  })
+
+  test('string', () => {
+    expect(sayHello('Alex')).toBe('Hello! Alex')
+  })
+
+  test('empty', () => {
+    expect(sayHello()).toBe('Hello! Stranger')
+  })
+})
