1e0ba68878da7fcfd2c554f038ded3922e4b5238
Alexander Kurganow <me@akurganow.ru>
2018-05-03 00:45:54 +0300

    feat(trainings): add combine-list

---
+++ src/combine-list/spec.js
@@ -0,0 +1,25 @@
+import resolve from './index'
+
+describe('Combine list', () => {
+  test('one item', () => {
+    expect(
+      resolve('one')
+    ).toBe('<ul><li class="item first only">one</li></ul>')
+  })
+
+  test('three items', () => {
+    expect(
+      resolve('one', 'two', 'three')
+    ).toBe('<ul>' +
+      '<li class="item first">one</li>' +
+      '<li class="item">two</li>' +
+      '<li class="item last">three</li>' +
+      '</ul>')
+  })
+
+  test('empty', () => {
+    expect(
+      resolve()
+    ).toBe('<ul><li class="item only empty">Empty list</li></ul>')
+  })
+})
